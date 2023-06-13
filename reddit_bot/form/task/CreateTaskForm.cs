694bf33a409bdacdeddcd472b4d705250be57371
vdolhushyn <vetajika@gmail.com>
2023-06-13 22:37:34 +0300

    going to vs

---
+++ reddit_bot/form/task/CreateTaskForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using reddit_bot.domain;
+
+namespace reddit_bot.form.task
+{
+    public partial class CreateTaskForm : Form
+    {
+        private readonly RedditAccount _redditAccount;
+        private readonly AccountsForm _accountsForm;
+        
+        public CreateTaskForm(RedditAccount redditAccount, AccountsForm accountsForm)
+        {
+            InitializeComponent();
+
+            _redditAccount = redditAccount;
+            _accountsForm = accountsForm;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
