1ccba73c94b7ff69f1b28dbed5aae05d82343fe4
vdolhushyn <vetajika@gmail.com>
2023-08-06 01:30:58 +0300

    (feater) added forgotten resources

---
+++ reddit_bot/form/preset/PoolSubredditChangeInfoForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace reddit_bor.form.preset
+{
+    partial class PoolSubredditChangeInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Cancel_Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 430);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ок";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Ok_Button_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.richTextBox1);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Controls.Add(this.radioButton3);
+            this.panel4.Controls.Add(this.radioButton2);
+            this.panel4.Controls.Add(this.radioButton1);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.label15);
+            this.panel4.Controls.Add(this.numericUpDown3);
+            this.panel4.Controls.Add(this.button10);
+            this.panel4.Controls.Add(this.label11);
+            this.panel4.Controls.Add(this.label10);
+            this.panel4.Controls.Add(this.comboBox3);
+            this.panel4.Controls.Add(this.label9);
+            this.panel4.Controls.Add(this.comboBox2);
+            this.panel4.Controls.Add(this.label8);
+            this.panel4.Location = new System.Drawing.Point(12, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(320, 412);
+            this.panel4.TabIndex = 13;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 294);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(320, 117);
+            this.richTextBox1.TabIndex = 28;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Додаткова інформація";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(133, 185);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(58, 17);
+            this.radioButton3.TabIndex = 26;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Кінець";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(64, 185);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.TabIndex = 25;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Середина";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(0, 185);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 24;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Початок";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Тригер";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(0, 233);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(161, 13);
+            this.label15.TabIndex = 21;
+            this.label15.Text = "Кількість можливих публікацій";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(6, 249);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(312, 20);
+            this.numericUpDown3.TabIndex = 20;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(212, 40);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(103, 29);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Пошук";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(3, 120);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(3, 45);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 9;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(-1, 96);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(319, 21);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(0, 80);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(138, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Виберіть флаєр для посту";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(0, 13);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(318, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label8.Location = new System.Drawing.Point(0, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(231, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Введіть назву сабреддіту. Напр. (SweNSFW)";
+            // 
+            // PoolSubredditChangeInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 468);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "PoolSubredditChangeInfoForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "PoolSubredditNewCountForm";
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ reddit_bot/form/preset/PoolSubredditChangeInfoForm.cs
@@ -0,0 +1,307 @@
+﻿using Reddit;
+using Reddit.Controllers;
+using Reddit.Exceptions;
+using reddit_bor.domain.pool;
+using reddit_bor.service;
+using reddit_bot.domain.forms;
+using reddit_bot.domain.task;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace reddit_bor.form.preset
+{
+    public partial class PoolSubredditChangeInfoForm : Form
+    {
+        private readonly RedditClient _redditClient;
+        private readonly SubredditService _subredditService;
+
+        public PoolSubreddit _poolSubreddit;
+
+        private Subreddit _subreddit;
+
+        public PoolSubredditChangeInfoForm(PoolSubreddit poolSubreddit, RedditClient redditClient)
+        {
+            InitializeComponent();
+
+            _redditClient = redditClient;
+
+            _poolSubreddit = poolSubreddit;
+
+            FillForm();
+        }
+
+        private void FillForm()
+        {
+            comboBox2.Text = _poolSubreddit.Name;
+            if (_poolSubreddit.Trigger != null)
+            {
+                textBox1.Text = _poolSubreddit.Trigger.Text;
+                switch(_poolSubreddit.Trigger.Place)
+                {
+                    case Place.Start:
+                        radioButton1.Checked = true;
+                        break;
+                    case Place.Middle:
+                        radioButton2.Checked = true;
+                        break;
+                    case Place.End:
+                        radioButton3.Checked = true;
+                        break;
+                }
+            }
+            numericUpDown3.Value = _poolSubreddit.Count;
+            richTextBox1.Text = _poolSubreddit.AdditionalInfo;
+
+            LoadSubreddit();
+            LoadFlairs();
+            LoadFlair(_poolSubreddit.PostFlair.Text);
+
+            comboBox3.Text = _poolSubreddit.PostFlair.Text;
+
+            FillSubredditComboBox();
+            FillSubredditFlairComboBox();
+        }
+
+        private void Ok_Button_Click(object sender, EventArgs e)
+        {
+            _poolSubreddit.Name = comboBox2.Text;
+            _poolSubreddit.Count = (int)numericUpDown3.Value;
+            _poolSubreddit.AdditionalInfo = richTextBox1.Text;
+
+            if (string.IsNullOrEmpty(_poolSubreddit.Name))
+            {
+                MessageBox.Show("Виберіть ім'я сабредіту");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                Trigger trigger = new Trigger(textBox1.Text);
+                if (radioButton1.Checked)
+                {
+                    trigger.Place = Place.Start;
+                }
+                else if (radioButton2.Checked)
+                {
+                    trigger.Place = Place.Middle;
+                }
+                else if (radioButton3.Checked)
+                {
+                    trigger.Place = Place.End;
+                }
+                else
+                {
+                    MessageBox.Show("Виберіть місце для тригеру");
+                    return;
+                }
+                _poolSubreddit.Trigger = trigger;
+            }
+            DialogResult = DialogResult.OK;
+        }
+
+        private void Cancel_Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void FillSubredditFlairComboBox()
+        {
+            comboBox3.SelectedIndexChanged += LoadFlairEvent;
+            comboBox3.TextChanged += ChangeFlairNameEvent;
+        }
+
+        private void FillSubredditComboBox()
+        {
+            comboBox2.SelectedIndexChanged += LoadFlairsEvent;
+            comboBox2.TextChanged += LoadSubredditEvent;
+            comboBox2.Click += LoadValuesToComboBoxEvent;
+
+            button10.Click += LoadSubredditsByNameEvent;
+            button10.Click += LoadFlairsEvent;
+        }
+
+        #region events
+        private void LoadSubredditsByNameEvent(object sender, EventArgs e)
+        {
+            LoadSubredditsByName();
+        }
+
+        private void LoadValuesToComboBoxEvent(object sender, EventArgs e)
+        {
+            LoadValuesToComboBox();
+        }
+
+        private void LoadSubredditEvent(object sender, EventArgs e)
+        {
+            LoadSubreddit();
+        }
+
+        private void LoadFlairsEvent(object sender, EventArgs e)
+        {
+            LoadFlairs();
+        }
+
+        private void LoadFlairEvent(object sender, EventArgs e)
+        {
+            LoadFlair(((ComboBox)sender).Text);
+        }
+
+        private void ChangeFlairNameEvent(object sender, EventArgs e)
+        {
+            ChangeFlairName(((Control)sender).Text);
+        }
+        #endregion
+
+        private void LoadSubredditsByName()
+        {
+            string subredditName = comboBox2.Text;
+            if (string.IsNullOrEmpty(subredditName))
+            {
+                label10.Text = "Введіть назву сабредіту";
+                return;
+            }
+
+            List<string> subreddits = new List<string>();
+            try
+            {
+                subreddits.AddRange(_redditClient.SearchSubredditNames(subredditName, exact: true).Select(s => s.Name).ToList());
+                _redditClient.SearchSubredditNames(subredditName, exact: false).Select(s => s.Name)
+                    .ToList().ForEach(s =>
+                    {
+                        if (!subreddits.Contains(s))
+                        {
+                            subreddits.Add(s);
+                        }
+                    });
+            }
+            catch (RedditNotFoundException)
+            {
+            }
+
+            comboBox2.Items.Clear();
+
+            if (subreddits.Count == 0)
+            {
+                Subreddit subredditToSearch = _redditClient.Subreddit(subredditName);
+                try
+                {
+                    subredditToSearch.About();
+                    label10.Text = "";
+                }
+                catch (Exception)
+                {
+                    label10.Text = "Жодних сабреддітів не знайдено";
+                }
+            }
+            else
+            {
+                foreach (var subreddit in subreddits)
+                {
+                    comboBox2.Items.Add(subreddit);
+                }
+                comboBox2.DroppedDown = true;
+
+                label10.Text = "";
+            }
+        }
+
+        private void LoadValuesToComboBox()
+        {
+            if (string.IsNullOrWhiteSpace(comboBox2.Text))
+            {
+                List<PoolSubreddit> existedSubreddits = _subredditService.FindAllSubreddits();
+                comboBox2.Items.Clear();
+                foreach (var subreddit in existedSubreddits)
+                {
+                    if (!comboBox2.Items.Contains(subreddit.Name))
+                    {
+                        comboBox2.Items.Add(subreddit.Name);
+                    }
+                }
+            }
+        }
+
+        private void LoadSubreddit()
+        {
+            _subreddit = _redditClient.Subreddit(comboBox2.Text);
+        }
+
+        private void LoadFlairs()
+        {
+            if (_subreddit == null)
+            {
+                return;
+            }
+
+            Flairs flairs = _subreddit.Flairs;
+
+            comboBox3.Items.Clear();
+            try
+            {
+                label11.Text = "";
+                comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+
+                List<Reddit.Things.FlairV2> flairList = flairs.LinkFlairV2;
+                foreach (var flair in flairList)
+                {
+                    comboBox3.Items.Add(new ComboBoxItem(flair.Text, flair.Id));
+                }
+
+                if (comboBox3.Items.Count == 0)
+                {
+                    label11.Text = "Для даного сабредіту флаєри не знайдено";
+                }
+                else
+                {
+                    label11.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+                label11.Text = "Флаєри не знайдено";
+            }
+        }
+
+        private void LoadFlair(string flairText)
+        {
+            if (_subreddit == null)
+            {
+                return;
+            }
+
+            Reddit.Things.FlairV2 flair = _subreddit.Flairs.GetLinkFlairV2()
+                .Where(f => f.Text.Equals(flairText))
+                .FirstOrDefault();
+
+            if (flair == null)
+            {
+                _poolSubreddit.PostFlair = null;
+                MessageBox.Show("Flair is null");
+                return;
+            }
+            _poolSubreddit.PostFlair = new PostFlair(flair.Text, flair.Id);
+
+            if (flair.TextEditable)
+            {
+                comboBox3.DropDownStyle = ComboBoxStyle.DropDown;
+            }
+            else
+            {
+                comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            }
+        }
+
+        private void ChangeFlairName(string postFlair)
+        {
+            if (_poolSubreddit.PostFlair != null)
+            {
+                _poolSubreddit.PostFlair.Text = postFlair;
+                return;
+            }
+
+            _poolSubreddit.PostFlair = new PostFlair(postFlair);
+        }
+    }
+}
